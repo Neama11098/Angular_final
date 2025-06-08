cf02e5f53e14fe5a966f57ff26346ee9afc03bb9
neama <neamasami953@gmail.com>
2025-06-08 15:11:38 +0200

    all

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { StudentModule } from './student/student.module';
+import { AdminModule } from './admin/admin.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AuthModule,
+    StudentModule,
+    AdminModule,
+    SharedModule,
+    AppComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { } 
